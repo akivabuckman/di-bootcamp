f6635f0d4a4b7a174b2db19b66470176bad837be
Akiva <akivabuckman@gmail.com>
2023-05-14 14:58:58 +0300

    w3d3_dc circle done

--- Week3/Day3/DailyChallenge/w3d3_dc_circle.py
+++ Week3/Day3/DailyChallenge/w3d3_dc_circle.py
@@ -1,3 +1,16 @@
+# Instructions :
+# The goal is to create a class that represents a simple circle.
+# A Circle can be defined by either specifying the radius or the diameter.
+# The user can query the circle for either its radius or diameter.
+#
+# Other abilities of a Circle instance:
+#
+# Compute the circle’s area
+# Print the circle and get something nice
+# Be able to add two circles together
+# Be able to compare two circles to see which is bigger
+# Be able to compare two circles and see if there are equal
+# Be able to put them in a list and sort them
 import math
 
 
