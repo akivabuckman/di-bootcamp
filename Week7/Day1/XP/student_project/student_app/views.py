f7f083ec21f1d04bb61748688ed8b0c43efc4995
Akiva <akivabuckman@gmail.com>
2023-06-11 21:09:37 +0300

    w7d1 dc done

--- Week7/Day1/XP/student_project/student_app/views.py
+++ Week7/Day1/XP/student_project/student_app/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from django.db.models import Q
+from datetime import datetime
+
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT
 
 
-# Create a student_list view for handling GET and POST requests
-# Handle GET request by retrieving all Student objects and serializing them
-# Handle POST request by saving data from the request and serializing it if valid
+class StudentDetailView(APIView):
+
+    def get(self, request, pk, *args, **kwargs):
+        student = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(student, data=request.data)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+    def put(self, request, pk, *args, **kwargs):
+        student = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, *args, **kwargs):
+        student = Student.objects.get(pk=pk)
+        student.delete()
+        return Response(HTTP_204_NO_CONTENT)
+
 
 class StudentListView(APIView):
     def get(self, request, *args, **kwargs):
-        queryset = Student.objects.all()
+        date_joined = self.request.query_params.get('date_joined')
+        if date_joined:
+            queryset = Student.objects.filter(date_joined__date=datetime.strptime(date_joined, "%Y-%m-%d").date())
+        else:
+            queryset = Student.objects.all()
         serializer = StudentSerializer(queryset, many=True)
         return Response(serializer.data)
 
@@ -23,22 +47,3 @@ class StudentListView(APIView):
             serializer.save()
             return Response(serializer.data, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-class StudentDetailView(APIView):
-    def put(self, request, pk, *args, **kwargs):
-        student = Student.objects.get(pk=pk)
-        serializer = StudentSerializer(student, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-
-    def get(self, request, pk, *args, **kwargs):
-        student = Student.objects.get(pk=pk)
-        serializer = StudentSerializer(student, data=request.data)
-        return Response(serializer.data)
-
-    def delete(self, request, pk, *args, **kwargs):
-        student = Student.objects.get(pk=pk)
-        student.delete()
-        return Response(HTTP_204_NO_CONTENT)
