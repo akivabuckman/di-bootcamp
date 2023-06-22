130c74ec625f94743ece0edc572671d59c24da52
Akiva <akivabuckman@gmail.com>
2023-06-22 13:32:39 +0300

    w7d5 beer done

---
+++ Week7/Day5/DC Beer/script.js
@@ -0,0 +1,36 @@
+let usernum = parseInt(prompt('Number:'));
+
+let j = 1
+let i=usernum;
+
+do {
+    if (i > j) {
+        console.log(`${i} bottles of beer on the wall`);
+        console.log(`${i} bottles of beer`);
+        if (j==1) {
+            console.log(`Take ${j} down, pass it around`);
+        } else {
+            console.log(`Take ${j} down, pass them around`);
+        }
+        if (i-j == 1) {
+            console.log(`Now you got ${i-j} bottle of beer on the wall`)
+        } else {
+            console.log(`Now you got ${i-j} bottles of beer on the wall`)
+        }
+        i -= j
+        j += 1
+    }
+}
+
+while (i > j);
+if (i == 1){
+    console.log('1 bottle of beer on the wall');
+    console.log('1 bottle of beer');
+    console.log('take it down, pass it around');
+    console.log('now go buy some more beer')
+} else {
+    console.log(`${i} bottles of beer on the wall`);
+    console.log(`${i} bottles of beer`);
+    console.log('take them down, pass them around');
+    console.log('now go buy some more beer')
+}
