1faf11d98b5c488c72d7e6df0fd39c9aa4de5f87
Akiva <akivabuckman@gmail.com>
2023-07-13 15:08:34 +0300

    w10d2 xp started

---
+++ Week10/Day2/XP/node-server/myserver.js
@@ -0,0 +1,18 @@
+// XP1
+// In this file, use http to create a server. You should return at least three different lines of HTML to the browser. (Use only HTML tags, no HTML files)
+// Your server should run on http://localhost:3000/
+
+const { log } = require("console");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write('<h1>Node.js is crazy</h1>');
+  res.write('<p>Not banging my head against a wall at all</p>');
+  res.write('<a href="https://www.google.com">Click here</a> to visit google.com');
+
+  // End the response
+  res.end();
+});
+
+server.listen(3000, () => {
+})
